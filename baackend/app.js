60d245a95e6141f3a33c9a25bf76ee097a93b17c
Hunain Siddiqui <hunain@Hunains-MacBook-Air.local>
2023-10-15 20:48:23 +0530

    refix cors

--- baackend/app.js
+++ baackend/app.js
@@ -10,7 +10,7 @@ app.use(express.json()) ;
 app.use(cookieParser()) ;
 app.use(cors({
   origin: ['http://localhost:3000', 'https://ecommersebackend1.onrender.com'],
-  methods: 'GET, POST,PUT,DELETESs',
+  methods: 'GET, POST,PUT,DELETE',
   credentials: true, // Include cookies in requests
 }));
 
